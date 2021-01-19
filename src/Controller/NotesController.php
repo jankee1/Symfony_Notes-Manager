dfeeed79455d18c1cc649b0af356f535e14c9525
Jan <janblaszczyk93@gmail.com>
2021-01-19 17:40:24 +0100

    19.01.2021_created_features:edit_and_delete

--- src/Controller/NotesController.php
+++ src/Controller/NotesController.php
@@ -7,27 +7,36 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Notes;
 
+
+#[Route('/notes_list', name: 'notes.')]
 class NotesController extends AbstractController
 {
-    #[Route('/notes_list/{noteId?}', name: 'notes_list', methods:['GET'])]
-    public function notes_list($noteId): Response
+    #[Route('/', name: 'list', methods:['GET'])]
+    public function notes_list(): Response
     {
-        // $note = [
-        //   'title' => 'this is title',
-        //   'description' => 'this is description',
-        //   'date' => 'this is date',
-        // ];
-
         $notes = new Notes();
         $em = $this->getDoctrine()->getManager();
         $em->persist($notes);
 
         $retreivedNotes = $em->getRepository(Notes::class)->findAll();
 
-        return $this->render('notes/index.html.twig', [
+        return $this->render('notes/notes_list.html.twig', [
             'controller_name' => 'NotesController',
-            'noteId' => $noteId,
             'note' => $retreivedNotes
         ]);
+    }
+    #[Route('/edit/{noteId?}', name: "edit_note")]
+    public function edit_note($noteId)
+    {
+      return $this->render('notes/edit_note.html.twig', [
+          'parameter' => $noteId,
+      ]);
+    }
+    #[Route('/delete/{noteId?}', name: "delete_note")]
+    public function delete_note($noteId)
+    {
+      return $this->render('notes/delete_note.html.twig', [
+          'parameter' => $noteId,
+      ]);
     }
 }
