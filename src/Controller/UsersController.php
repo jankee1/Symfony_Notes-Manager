f9961588bc0b36ac8de1ceb3aebba4c3a5994a95
Jan <jan.blaszczyk@protonmail.com>
2021-01-30 13:56:24 +0100

    30.01.2021_added_features:users_entity_and_users_controller

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Users;
+
+class UsersController extends AbstractController
+{
+    #[Route('/', name: 'users')]
+    public function index(): Response
+    {
+        return $this->render('users/index.html.twig', [
+            'controller_name' => 'UsersController',
+        ]);
+    }
+}
